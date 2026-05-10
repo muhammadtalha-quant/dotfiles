8782a370ab1ae04cf5d4e59b0430b459fa1e7cb8
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-05-10 17:19:05 +0500

    updated completions

--- fish/completions/compress.fish
+++ fish/completions/compress.fish
@@ -1,6 +1,2 @@
-complete -c compress -l help -f -d 'Show usage'
-# First arg: suggest output archive name with format hints
-complete -c compress -f -n "test (count (commandline -opc)) -lt 2" \
-    -d 'Output archive (e.g. out.tar.gz, out.zip, out.tar.xz)'
-# Subsequent args: any file/dir
-complete -c compress -F -n "test (count (commandline -opc)) -ge 2"
+complete -c compress -s h -l help    -f -d 'Show usage'
+complete -c compress -s v -l verbose -f -d 'Show files as they are compressed'
