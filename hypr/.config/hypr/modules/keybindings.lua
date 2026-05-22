7c878ce74a815d38b6570a2811fd983c1f9ac060
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-05-22 21:58:39 +0500

    refactor: QoL improvements for maintainability

--- hypr/.config/hypr/modules/keybindings.lua
+++ hypr/.config/hypr/modules/keybindings.lua
@@ -1,63 +1,150 @@
+-------------------------------------------------------------
+---                        KEYS                           ---
+-------------------------------------------------------------
+
+local function chord(...)
+    return table.concat({...}, "+")
+end 
+
+local  KEYS = {
+    MODIFIER = {
+        SUPER = "SUPER",
+        CTRL = "CONTROL",
+        ALT = "ALT",
+        SHIFT = "SHIFT"
+    },
+    ALPHABET = {
+        A = "A",
+        B = "B",
+        C = "C",
+        D = "D",
+        E = "E",
+        F = "F",
+        G = "G",
+        H = "H",
+        I = "I",
+        J = "J",
+        K = "K",
+        L = "L",
+        M = "M",
+        N = "N",
+        O = "O",
+        P = "P",
+        Q = "Q",
+        R = "R",
+        S = "S",
+        T = "T",
+        U = "U",
+        V = "V",
+        W = "W",
+        X = "X",
+        Y = "Y",
+        Z = "Z"
+    },
+    ARROW = {
+        LEFT = "LEFT",
+        RIGHT = "RIGHT",
+        UP = "UP",
+        DOWN = "DOWN",
+    },
+    NUMBER = {
+        ONE = "1",
+        TWO = "2",
+        THREE = "3",
+        FOUR = "4",
+        FIVE = "5",
+        SIX = "6",
+        SEVEN = "7",
+        EIGHT = "8",
+        NINE = "9",
+        ZERO = "0",
+    },
+    PUNCTUATION = {
+        COMMA = "COMMA",
+        PERIOD = "PERIOD",
+        SEMICOLON = "SEMICOLON",
+        APOSTROPHE = "APOSTROPHE",
+    },
+    SYMBOL = {
+        EQUAL = "EQUAL",
+        MINUS = "MINUS",
+        BRACKETRIGHT = "BRACKETRIGHT",
+        BRACKETLEFT = "BRACKETLEFT",
+        SLASH = "SLASH",
+        BACKSLASH = "BACKSLASH",
+        BACKTICK = "GRAVE"
+    },
+    SPECIAL = {
+        BACKSPACE  = "BACKSPACE",
+        ENTER = "RETURN",
+        SPACE = "SPACE",
+        TAB = "TAB"
+    },
+    NAVIGATION = {
+        INSERT = "INSERT",
+        DELETE = "DELETE",
+        HOME = "HOME",
+        END = "END",
+        PGUP = "PRIOR",
+        PGDN = "NEXT",
+    },
+    FUNCTION = {
+        F1 = "F1",
+        F2 = "F2",
+        F3 = "F3",
+        F4 = "F4",
+        F5 = "F5",
+        F6 = "F6",
+        F7 = "F7",
+        F8 = "F8",
+        F9 = "F9",
+        F10 = "F10",
+        F11 = "F11",
+        F12 = "F12",
+    },
+    XF86 = {
+        HOMEPAGE = "XF86HomePage",
+        MAIL = "XF86Mail",            
+        SEARCH = "XF86Search",        
+        TOOLS = "XF86Tools",        
+        AUDIOPLAY = "XF86AudioPlay",        
+        AUDIOPREV = "XF86AudioPrev",        
+        AUDIONEXT = "XF86AudioNext",        
+        AUDIOLOWERVOLUME = "XF86AudioLowerVolume",        
+        AUDIORAISEVOLUME = "XF86AudioRaiseVolume",        
+        AUDIOMUTE = "XF86AudioMute",        
+        EXPLORER = "XF86Explorer",        
+        CALCULATOR = "XF86Calculator"        
+    },
+    MOUSE = {
+        LMB = "mouse:272",
+        RMB = "mouse:273",
+        MMB = "mouse:274"
+    }, 
+}
+
+
+
+
 -----------------------------------------------------------
 ---                       APPS                          ---
 -----------------------------------------------------------
 
 
-local terminal = "ghostty"
-local browser = "google-chrome-stable"
-local vscode = "code"
-local explorer = "dolphin"
+local apps = {
+    ghostty = chord(KEYS.MODIFIER.SUPER, KEYS.SPECIAL.ENTER),
+    ["google-chrome-stable"] = chord(KEYS.MODIFIER.SUPER, KEYS.ALPHABET.B),
+    code = chord(KEYS.MODIFIER.SUPER, KEYS.ALPHABET.C),
+    dolphin = chord(KEYS.MODIFIER.SUPER, KEYS.ALPHABET.E),
+    localsend = chord(KEYS.MODIFIER.SUPER, KEYS.ALPHABET.L)
+}
 
-
-
--------------------------------------------------------------
----                        KEYS                           ---
--------------------------------------------------------------
-
-local function join_keys(key_a, key_b) 
-    return key_a .. "+" .. key_b
+for command,keybind in pairs(apps) do 
+    hl.bind(
+        keybind,
+        hl.dsp.exec_cmd(command)
+    )
 end
-
-local super = "SUPER"
-local control = "CONTROL"
-local shift = "SHIFT"
-local alt = "ALT"
-local enter = "RETURN"
-
-local arrow_left = "LEFT"
-local arrow_right = "RIGHT"
-local arrow_up = "UP"
-local arrow_down = "DOWN"
-
-local left_click = "mouse:272"
-local right_click = "mouse:273"
-local middle_click = "mouse:274"
-
-
--------------------------------------------------------------
----                    LAUNCH APPS                        ---   
--------------------------------------------------------------
-
-hl.bind(
-    join_keys(super,enter),
-    hl.dsp.exec_cmd(terminal)
-)
-
-hl.bind(
-    join_keys(super,"B"),
-    hl.dsp.exec_cmd(browser)
-)
-
-hl.bind(
-    join_keys(super,"E"),
-    hl.dsp.exec_cmd(explorer)
-)
-
-hl.bind(
-    join_keys(super,"C"),
-    hl.dsp.exec_cmd(vscode)
-)
-
 
 -------------------------------------------------------------
 ---                 WORKING WITH WINDOWS                  ---   
@@ -65,46 +152,74 @@ hl.bind(
 
 
 hl.bind( -- close single window
-    join_keys(super,"Q"),
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.ALPHABET.Q
+    ),
     hl.dsp.window.close()
 )
 
 hl.bind( -- close all instances of a window
-    join_keys(join_keys(super,shift), "Q"),
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.MODIFIER.SHIFT,
+        KEYS.ALPHABET.Q
+    ),
     hl.dsp.window.kill()
 )
 
 hl.bind( -- toggle float / tile
-    join_keys(super,"F"),
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.ALPHABET.F
+    ),
     hl.dsp.window.float()
 )
 
 hl.bind( -- focus left
-    join_keys(super,arrow_left),
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.ARROW.LEFT
+    ),
     hl.dsp.focus({
         direction = "left"
     })
 )
 
 hl.bind( -- focus right
-    join_keys(super,arrow_right),
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.ARROW.RIGHT
+    ),
     hl.dsp.focus({
         direction = "right"
     })
 )
 hl.bind( -- focus up
-    join_keys(super,arrow_up),
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.ARROW.UP
+    ),
     hl.dsp.focus({
         direction = "up"
     })
 )
 
 hl.bind( -- focus down
-    join_keys(super,arrow_down),
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.ARROW.DOWN
+    ),
     hl.dsp.focus({
         direction = "down"
     })
 )
+
+hl.bind( -- toggle fullscreen
+    chord(KEYS.FUNCTION.F11),
+    hl.dsp.window.fullscreen()
+)
+
 
 -------------------------------------------------------------
 ---                WORKING WITH WORKSPACES                ---   
@@ -112,7 +227,10 @@ hl.bind( -- focus down
 
 for i = 1, 9 do
     hl.bind(
-        join_keys(super,tostring(i)),
+        chord(
+            KEYS.MODIFIER.SUPER,
+            tostring(i)
+        ),
         hl.dsp.focus({
             workspace = tostring(i)
         })
@@ -121,8 +239,9 @@ end
 
 for i = 1, 9 do
     hl.bind(
-        join_keys(
-            join_keys(super,shift),
+        chord(
+            KEYS.MODIFIER.SUPER,
+            KEYS.MODIFIER.SHIFT,
             tostring(i)
         ),
         hl.dsp.window.move({
@@ -133,8 +252,9 @@ end
 
 for i = 1, 9 do
     hl.bind(
-        join_keys(
-            join_keys(super,alt),
+        chord(
+            KEYS.MODIFIER.SUPER,
+            KEYS.MODIFIER.ALT,
             tostring(i)
         ),
         hl.dsp.window.move({
@@ -145,9 +265,10 @@ for i = 1, 9 do
 end
 
 hl.bind(
-    join_keys(
-        join_keys(super,control),
-        arrow_right
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.MODIFIER.ALT, 
+        KEYS.ARROW.RIGHT
     ),
     hl.dsp.focus({
         workspace = "+1"
@@ -155,9 +276,10 @@ hl.bind(
 )
 
 hl.bind(
-    join_keys(
-        join_keys(super,control),
-        arrow_left
+    chord(
+        KEYS.MODIFIER.SUPER,
+        KEYS.MODIFIER.ALT,
+        KEYS.ARROW.LEFT
     ),
     hl.dsp.focus({
         workspace = "-1"
