057e3785cce76759d7bec0c4fb7d81a2e7e7018d
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-05-23 22:45:41 +0500

    made hyprland.lua v5 aware

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -1,7 +1,8 @@
 require("modules.keybindings")
 
 local function start_shell()
-	hl.exec_cmd("qs -c noctalia-shell")
+	hl.exec_cmd("qs -c noctalia-shell") -- to be removed when noctalia v5 releases
+	-- hl.exec_cmd("noctalia") -- to be uncommented when noctalia v5 releases
 end
 
 hl.on("hyprland.start", start_shell)
